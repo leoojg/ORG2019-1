f932639d23a14ef391220e0c93b55bc5b88820ae
Leonardo Gonçalves <leoojg@gmail.com>
2019-06-03 01:07:52 -0300

    imprime campos implementada, mas com erros para correção

--- campo_minado/campominado.asm
+++ campo_minado/campominado.asm
@@ -3,14 +3,25 @@
 	difficultyMessage:	.asciiz "a)5x5   b)7x7   c)9x9\nEscolha entre uma dificuldade acima: "
 	difficultyMessageError: .asciiz "\n\n\n\nESCOLHA UMA OPCAO VALIDA!!\n"
 	difficultyData:		.space 5			## Utilizado para leitura da dificuldade
-	startMessage:		.asciiz "\n\n\n\n\nComeco do jogo:\n"
-	lineMessage:		.asciiz "Escolha uma linha para começar(a linha precisa ser valida de acordo com a dificuldade): "
+	startMessage:		.asciiz "\nComeco do jogo:\n"
+	lineMessage:		.asciiz "Escolha uma linha(a linha precisa ser valida de acordo com a dificuldade): "
 	columnMessage:		.asciiz "Agora escolha a coluna(a coluna precisa ser valida de acordo com a dificuldade): "
-	
-	
-	barran:			.asciiz "\n"
+	getLineColumnMessage:	.asciiz "\n\nInsira uma opcao valida!!\n"
+	alreadyChoseMessage:	.asciiz "\nVoce ja escolheu essa posicao!!\n"
+	newMoveMessage:		.asciiz "Nova jogada!!\n\n"
+	explosionMessage:	.asciiz "\nUma bomb explodiu!!\n\n"
+	winningMessage:		.asciiz "\nParabéns, você ganhou!!\n"
+	endGameMessage:		.asciiz "\nFim de jogo"
 
-	explosionMessage:	.asciiz "Uma bomb explodiu!!\n\n\nFim de jogo"
+	openFields:		.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
+				.word 0 0 0 0 0 0 0 0 0
 
 	campo:			.word 0 0 9 0 9 0 0 0 0
 				.word 0 0 0 0 9 0 0 0 0
@@ -24,6 +35,12 @@
 .text
 
 	main:							###  Inicio do programa
+		la $a0, campo
+		la $a1, openFields
+		li $a2, 9
+		jal mostra_campo
+		j fim
+		##############
 		li $v0, 4
 		la $a0, initMessage
 		syscall
@@ -51,29 +68,102 @@
 		li $s7, 9
 		beq $t0, $t1, check
 
-	check:										## Verifica se o que foi digitado tem um \n na sequencia
-		addi $s0, $s0, 1
-		lb $t1, ($s0)
-		la $t0, '\n'
-		beq $t0, $t1, game
-
 	goBack:
 		li $v0, 4
 		la $a0, difficultyMessageError
 		syscall
 		j difficulty								## Volta pra leitura da dificuldade, pois a inserida foi invalida.
 
+	check:										## Verifica se o que foi digitado tem um \n na sequencia
+		addi $s0, $s0, 1
+		lb $t1, ($s0)
+		la $t0, '\n'
+		beq $t0, $t1, game
+
 	game:
 		li $v0, 4
 		la $a0, startMessage
 		syscall
 		
-		la $a0, campo
-		move $a1, $s7
+		la $a0, campo								## Carrega endereço de campo em $a0
+		move $a1, $s7								## Coloca a ordem da matriz campo em $a1
 		jal calcula_bombas
-				
+		j getLineColumn
+
+	lineColumnMessageError:
+		li $v0, 4
+		la $a0, getLineColumnMessage
+		syscall
+		j getLineColumn
+	lineColumnAlreadyChose:
+		li $v0, 4
+		la $a0, alreadyChoseMessage
+		syscall
+	getLineColumn:
+		li $v0, 4
+		la $a0, lineMessage
+		syscall
+		li $v0, 5
+		syscall									## Lê i
+		move $t0, $v0
+		li $v0, 4
+		la $a0, columnMessage
+		syscall									## Lê j
+		li $v0, 5
+		syscall
+		move $t1, $v0
+											## Verifica se i e j são validos de acordo com a dificuldade
+		addi $t6, $s7, 1
+		slt $t5, $zero, $t0
+		beq $zero, $t5, lineColumnMessageError
+		slt $t5, $zero, $t1
+		beq $zero, $t5, lineColumnMessageError
+		slt $t5, $t0, $t6
+		beq $zero, $t5, lineColumnMessageError
+		slt $t5, $t1, $t6
+		beq $zero, $t5, lineColumnMessageError
+		
+		addi $t0, $t0, -1
+		addi $t1, $t1, -1
+		mul $t0, $t0, 36
+		mul $t1, $t1, 4
+		add $t3, $t0, $t1							## Offset de [i][j] na matriz
+		
+		
+		la $t0, campo
+		la $t1, openFields
+		add $t4, $t1, $t3
+		lw $t5, ($t4)
+		bne $t5, $zero, lineColumnAlreadyChose					## Volta para leitura de i, j pois esse campo ja foi escolhido
+		li $t5, 1
+		sw $t5, ($t4)								## Seta openFields[i][j] para 1
+		add $t4, $t0, $t3
+		lw $t5, ($t4)
+		beq $t5, 9, fim
+		
+		li $v0, 4
+		la $a0, newMoveMessage							## Mensagem para realizar outra jogada
+		syscall
+		
+		move $a0, $t0								## Carrega endereço de campo em $a0
+		move $a1, $t1								## Carrega endereço de openFields em $a1
+		move $a2, $s7								## Coloca a ordem da matriz campo em $a2
+		#jal mostra_campo
+		#j getLineColumn
+		
+	
 
 	fim:
+		li $v0, 4
+		la $a0, explosionMessage
+		#syscall
+		move $a0, $t0								## Carrega endereço de campo em $a0
+		move $a1, $t1								## Carrega endereço de openFields em $a1
+		move $a2, $s7								## Coloca a ordem da matriz campo em $a2
+		#jal mostra_campo
+		li $v0, 4
+		la $a0, endGameMessage
+		syscall
 		
 	## Fim da main
 	li $v0, 10
@@ -156,24 +246,54 @@
 				sw $t6, ($t2)
 				isBomb:
 
-				lw $t5, ($t2)
-				li $v0, 1
-				move $a0, $t5
-				syscall
-
 				addi $t2, $t2, 4					## offset += 4
 				addi $t4, $t4, 1					## j += 1
 				j loop_j
 			fimLoop_j:
-
-			li $v0, 4
-			la $a0, barran
-			syscall
 			
-			mul $t5, $t3, 36
-			add $t5, $t5, $t0
-			move $t2, $t5
+			mul $t5, $t3, 36						## $t5 = offset em linhas
+			add $t2, $t5, $t0						## $t2 += endereço campo, atualiza offset na matriz
 			addi $t3, $t3, 1
 			j loop_i
 		fimLoop_i:	
+	jr $ra
+	
+	mostra_campo:
+		move $t0, $a0								## $t0 = endereco da matriz campo
+		move $t1, $a1								## $t1 = ordem da matriz campo
+		li $t2, 0								## Offset geral setado pra 0
+		li $t3, 0								## $t3 -> i = 0
+		loop_i_:
+			beq $a2, $t3, fimLoop_i_
+			li $t4, 0							## $t4 -> j = 0
+			loop_j_:
+				beq $a2, $t4, fimLoop_j_
+				add $t5, $t0, $t2					## Offset em campo setado
+				add $t6, $t1, $t2					## Offset em openFields setado
+				lw $t7, ($t6)
+				beq $t7, $zero, printx					## se openField[i][j] for 0 vai para print0
+				lw $t7, ($t5)						## lw em campo[i][j]
+				li $v0, 1
+				move $a0, $t7
+				syscall
+				j printSpace
+				printx:
+				li $v0 11
+				li $a0 'X'
+				syscall
+				printSpace:
+				li $v0 11
+				li $a0, ' '
+				syscall
+				addi $t4, $t4, 1
+				addi $t2, $t2, 4
+				j loop_j_
+			fimLoop_j_:
+			li $a0, '\n'
+			syscall
+			mul $t4, $t3, 36
+			add $t2, $t2, $t4
+			addi $t3, $t3, 1
+			j loop_i_		
+		fimLoop_i_:
 	jr $ra
